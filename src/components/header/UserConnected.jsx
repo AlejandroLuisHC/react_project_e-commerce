d0610334bcfd21d2cd71491fc6c2ab91ea442e90
AlejandroLHC <alejandrolhc@gmail.com>
2022-11-17 15:01:10 +0100

    fix(turn window.location into Navigate)

--- src/components/header/UserConnected.jsx
+++ src/components/header/UserConnected.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import { PersonCircle } from 'react-bootstrap-icons';
 import { BoxArrowLeft } from 'react-bootstrap-icons';
 import UserContext from '../../context/UserContext';
@@ -15,14 +15,19 @@ const UserConnected = ({ user }) => {
     }
     const { username } = user;
     const { userDispatch } = useContext(UserContext);
+    const [sendHome, setSendHome] = useState(null)
+    useEffect(()=>{
+        setSendHome(prev => prev = null)
+    }, [sendHome])
     const closeSession = () => {
         userDispatch({ type: 'RESET' });
-        window.location.replace('/') 
+        setSendHome(prev => prev = true)
     }
     return (
         <div className='d-flex align-items-center justify-content-start gap-3'>
             <Link to={username !== "Guest" ? "profile" : "login"} style={style}><span style={{fontSize: "30px"}}><PersonCircle/></span> {username !== "Guest" ? username : "Log in"}</Link>
             <button className="btn btn-outline-danger btn-sm d-flex justify-content-center align-items-center" onClick={closeSession}><BoxArrowLeft/></button>
+            {sendHome && <Navigate to="/" replace={true}/>}
         </div>
     )
 }
